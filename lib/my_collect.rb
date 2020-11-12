1544380bf2c912dae7df44b2c6c884a463bc4a27
MichaelAbe <michael.k.abe@gmail.com>
2020-11-12 15:25:05 -0500

    commit tree f65e29b00a478e6a2201ea88adb21002257c285e

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(stuff)
+    i = 0
+    array = []
+    while i < stuff.length
+        array. << yield stuff[i]
+        i = i + 1 
+    end
+    array
+end
 
-
+# def my_each(element)
+#     i = 0
+#     while i < element.length
+#       yield element[i]
+      
+#       i = i + 1
+#     end
+#     element
+# end
